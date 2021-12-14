64333ce560f7e4a88d566862197440f31f6c111c
Matt Smith <matt.smith@mediakind.com>
2021-12-14 19:57:25 +0000

    working pointless board

--- ui/src/components/pointless-tower/index.jsx
+++ ui/src/components/pointless-tower/index.jsx
@@ -1,14 +1,33 @@
+import "./style.scss";
 
 const PointlessTower = (props) => {
     const {height, level, target} = props;
     
     return (
         <div className="pointless-tower">
-            {Array(height).fill().map((_, id) => (
-                <div className={`level ${target == id ? "target" : ""} ${level < id ? "hidden" : ""}`} key={id}/>
-            ))}
+            <div className="counter">{level}</div>
+            <div className="tower">
+                {Array(height).fill().map((_, id) => (
+                    <div className={`level ${target == (height-id) ? "target" : ""} ${level < (height-id) ? "hidden" : ""}`} 
+                        key={height-id}
+                        style={{background: `linear-gradient(90deg, rgba(150,150,150,1) 0%, rgba(255,255,255,1) ${lerp(23, 42,id, height)}%, rgba(255,255,255,1) ${lerp(75, 45, id, height)}%, rgba(150,150,150,1) 100%)`}}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
 
+function lerp (start, end, currentStep, finalStep){
+    // Current %
+    const progress = currentStep / finalStep;
+    let trip;
+    if (end > start) {
+        trip = end - start
+    } else {
+        trip = start -end;
+    }
+    const distance = start + (trip * progress);
+    return distance;
+  }
 export default PointlessTower;
--- ui/src/pages/pointless/index.jsx
+++ ui/src/pages/pointless/index.jsx
@@ -1,10 +1,32 @@
 import React from "react"
 
 import PointlessTower from "../../components/pointless-tower";
+import "./style.scss";
 
 class Pointless extends(React.Component) {
     state = {
-        teams: [],
+        teams: [
+            {
+                name: "Sam, Katie & Mary",
+                score: 42,
+                roundScore: 0
+            },
+            {
+                name: "Mike, Jill & Liz",
+                score: 12,
+                roundScore: 1
+            },
+            {
+                name: "Vija, Nick and Adam",
+                score: 121,
+                roundScore: 2
+            }
+        ],
+        tower: {
+            height: 100,
+            level: 100,
+            target: 32
+        },
         showTower: true,
         selectedRound: null,
         rounds: {
@@ -28,20 +50,68 @@ class Pointless extends(React.Component) {
             }
         }
     }
+    incorrectAudio = new Audio("/sfx/pointless-incorrect.mp3");
+    componentDidMount() {
+        window.game = this;
+    }
+
     render = () => {
         return (
             <div className="pointless-board">
-                Pointless
+                
                 <div className="players">
-                    {this.state.teams.map(team => (
-                        <div>
-                            {team.name} : {team.score}
-                        </div>
-                    ))}
+                    <h1>Teams</h1>
+                    <div className="teams-container">
+                        {this.state.teams.map(team => (
+                            <div className="team-container">
+                                <h2>{team.name}</h2>
+                                <h3>Score: {team.score}</h3>
+                                <div className="score-container">
+                                    {Array(team.roundScore).fill().map((_, score) => (
+                                        <div className="score-icon">&nbsp;</div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <PointlessTower level={100} target={40} height={100}/>
+                <div className="tower-container">
+                    <PointlessTower level={this.state.tower.level} target={this.state.tower.target} height={this.state.tower.height}/>
+                </div>        
             </div>
         )
+    }
+
+    async countDown(target) {
+        for (let i = 100; i > target; i--) {
+            this.decrementTower();
+            await this.sleep(50);
+        }
+        this.incorrectAudio.play();
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    decrementTower(){
+        this.setState((prevState) => ({
+            tower: {
+                level: prevState.tower.level - 1,
+                height: this.state.tower.height,
+                target: this.state.tower.target
+            }
+        }))
+    }
+
+    reset() {
+        this.setState({
+            tower: {
+                level: 100,
+                target: null,
+                height: this.state.tower.height
+            }
+        });
     }
 
     addTeam = (name) => {
