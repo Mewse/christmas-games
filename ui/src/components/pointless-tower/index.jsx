5aea275861d97833c86c74b0f63b111298f2b757
Matt Smith <matt.smith@mediakind.com>
2021-12-14 23:53:18 +0000

    More sfx, style changes

--- ui/src/components/pointless-tower/index.jsx
+++ ui/src/components/pointless-tower/index.jsx
@@ -8,9 +8,9 @@ const PointlessTower = (props) => {
             <div className="counter">{level}</div>
             <div className="tower">
                 {Array(height).fill().map((_, id) => (
-                    <div className={`level ${target == (height-id) ? "target" : ""} ${level < (height-id) ? "hidden" : ""}`} 
+                    <div className={`level ${target === (height-id) ? "target" : ""} ${level < (height-id) ? "hidden" : ""} ${level === height-id ? "top": ""}`} 
                         key={height-id}
-                        style={{background: `linear-gradient(90deg, rgba(150,150,150,1) 0%, rgba(255,255,255,1) ${lerp(23, 42,id, height)}%, rgba(255,255,255,1) ${lerp(75, 45, id, height)}%, rgba(150,150,150,1) 100%)`}}
+                        // style={{background: `linear-gradient(90deg, rgba(150,150,150,1) 0%, rgba(255,255,255,1) ${lerp(23, 42,id, height)}%, rgba(255,255,255,1) ${lerp(75, 45, id, height)}%, rgba(150,150,150,1) 100%)`}}
                     />
                 ))}
             </div>
--- ui/src/pages/pointless/index.jsx
+++ ui/src/pages/pointless/index.jsx
@@ -51,8 +51,13 @@ class Pointless extends(React.Component) {
         }
     }
     incorrectAudio = new Audio("/sfx/pointless-incorrect.mp3");
+    targetReachedAudio = new Audio("/sfx/pointless-target-reached.mp3");
+    correctAnswer = new Audio("/sfx/pointless-correct-answer.mp3");
+    answerEnd = new Audio("/sfx/pointless-answer-end.mp3");
     componentDidMount() {
         window.game = this;
+        this.targetReachedAudio.volume = 0.1;
+        this.answerEnd.volume = 0.5;
     }
 
     render = () => {
@@ -75,18 +80,34 @@ class Pointless extends(React.Component) {
                         ))}
                     </div>
                 </div>
-                <div className="tower-container">
-                    <PointlessTower level={this.state.tower.level} target={this.state.tower.target} height={this.state.tower.height}/>
-                </div>        
+                {this.state.showTower ? (
+                    <div className="tower-container">
+                        <PointlessTower level={this.state.tower.level} target={this.state.tower.target} height={this.state.tower.height}/>
+                    </div>        
+                ) : (
+                    <div className="round-container">
+
+                    </div>
+                )}
             </div>
         )
     }
 
     async countDown(target) {
+        this.correctAnswer.play();
         for (let i = 100; i > target; i--) {
+            if (i === this.state.tower.target) {
+                this.targetReachedAudio.play();
+            }
             this.decrementTower();
             await this.sleep(50);
         }
+        this.answerEnd.play();
+        this.correctAnswer.pause();
+        this.correctAnswer.currentTime = 0;
+    }
+
+    fail() {
         this.incorrectAudio.play();
     }
 
@@ -116,7 +137,7 @@ class Pointless extends(React.Component) {
 
     addTeam = (name) => {
         this.setState({
-            teams: this.state.teams.append({name: name, score: 0})
+            teams: this.state.teams.append({name: name, score: 0, roundScore: 0})
         })
     }
 }
