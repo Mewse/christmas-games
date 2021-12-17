c76072f0a8f0076c3ba167f11388c193dea6520c
Matt Smith <matt.smith@mediakind.com>
2021-12-17 05:21:59 +0000

    Add tenable sounds

--- ui/src/components/tenable-tower/index.jsx
+++ ui/src/components/tenable-tower/index.jsx
@@ -1,14 +1,14 @@
 import "./style.scss";
 
 const TenableTower = (props) => {
-    let {answers, answered, fail} = props;
+    let {answers, answered, fail, highlight} = props;
     return (
         <div className={`tenable-tower ${fail ? "fail": ""}`}>
             <div className="answer-container answer-clip">
                 {answers.map((answer, id) => (
                     <div 
                         style={{width: 300+ 60*(id+1)}}
-                        className={`answer ${answered.includes(answer) ? "answered": ""}`}>
+                        className={`answer ${answered.includes(answer) ? "answered": ""} ${highlight === id ? "highlight": ""}`}>
                             {answer}
                     </div>
                 ))}
--- ui/src/pages/tenable/index.jsx
+++ ui/src/pages/tenable/index.jsx
@@ -6,13 +6,28 @@ import TenableTower from "../../components/tenable-tower";
 class Tenable extends React.Component {
     state = {
         answers: ["1","2","3","4","5","6","7","8","9","10"],
-        answered: [],
-        fail: false
+        answered: ["2", "7", "8"],
+        fail: false,
+        highlight: null
     }
     
+    stepSounds = [
+        new Audio("/sfx/tenable-step-1.mp3"),
+        new Audio("/sfx/tenable-step-2.mp3"),
+        new Audio("/sfx/tenable-step-3.mp3"),
+        new Audio("/sfx/tenable-step-4.mp3"),
+        new Audio("/sfx/tenable-step-5.mp3"),
+        new Audio("/sfx/tenable-step-6.mp3"),
+        new Audio("/sfx/tenable-step-7.mp3"),
+        new Audio("/sfx/tenable-step-8.mp3"),
+        new Audio("/sfx/tenable-step-9.mp3"),
+    ]
+    correctSound = new Audio("/sfx/tenable-correct-answer.mp3");
+    failSound = new Audio("/src/tenable-fail.mp3");
+
     componentDidMount() {
         window.game = this;
-        document.addEventListener("keypress", (e) => {
+        this.listener = document.addEventListener("keypress", (e) => {
             if (e.code === "KeyF") {
                 this.showIncorrect();
             }
@@ -24,7 +39,7 @@ class Tenable extends React.Component {
     }
 
     componentWillUnmount() {
-
+        document.removeEventListener("keypress", this.listener);
     }
 
     render() {
@@ -34,17 +49,46 @@ class Tenable extends React.Component {
                     answers={this.state.answers}
                     answered={this.state.answered}
                     fail={this.state.fail}
+                    highlight={this.state.highlight}
                 />
             </div>
         );
     }
 
-    showIncorrect() {
-
+    setHighlight(index) {
+        this.setState({
+            highlight: index
+        });
     }
 
-    showCorrect(index) {
+    async showIncorrect() {
+        await this.stepToIndex(0);
+        this.failSound.play();
+        this.setState({
+            highlight: null,
+            fail: true
+        })
+    }
 
+    async showCorrect(index) {
+        await this.stepToIndex(index+1);
+        this.correctSound.play();
+        this.setState({
+            answered: this.state.answered.concat(this.state.answers[index]),
+            highlight: null,
+        })
+    }
+
+    async stepToIndex(index) {
+        for (let i = this.state.answers.length-1; i >= index; i--) {
+            this.setHighlight(i);
+            this.stepSounds[this.state.answers.length - (i+1)].play();
+            await this.sleep(800);
+        }
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
 
