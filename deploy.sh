f27610cede4b051df9727f01ff0f6f517dcef694
Matt Smith <matt.smith@mediakind.com>
2021-12-27 06:07:44 +0000

    Remove spreadsheet loading

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ cd client
 npm run build
 cd ..
 rm -rf server/build
-cp -rf client/build server/build
+cp -rf ui/build server/build
 cd server
 gcloud app deploy
 cd ..
--- ui/src/pages/home/index.jsx
+++ ui/src/pages/home/index.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "./style.scss";
-import Tabletop from "tabletop";
+// import Tabletop from "tabletop";
 import _ from "underscore";
-import ScoreTable from "../../components/score-table";
+// import ScoreTable from "../../components/score-table";
 
 const Home = (props) => {
 
@@ -15,20 +15,20 @@ const Home = (props) => {
         "adam", "liz", "mary", "vija", "nick"
     ]);
 
-    useEffect(() => {
-        Tabletop.init({
-            key: "https://docs.google.com/spreadsheets/d/19g_Ku-CjmuQvPBzKDyLN3AFPpB8ucCxfGXUWxKwlOwY/edit#gid=0",
-            simpleSheet: true
-        })
-        .then(data => setData(data))
-        .catch(err => console.warn(err))
-    }, [])
+    // useEffect(() => {
+    //     Tabletop.init({
+    //         key: "https://docs.google.com/spreadsheets/d/19g_Ku-CjmuQvPBzKDyLN3AFPpB8ucCxfGXUWxKwlOwY/edit#gid=0",
+    //         simpleSheet: true
+    //     })
+    //     .then(data => setData(data))
+    //     .catch(err => console.warn(err))
+    // }, [])
     
     return (
         <div className="home fullscreen-background">
             {_.range(100).map(i => <div class="snow"></div>)}
             <h1 className="site-title glow">The Christmas Channel</h1>
-            <ScoreTable players={players} data={data} />
+            {/* <ScoreTable players={players} data={data} /> */}
             <div className="game-bar">
                 <Link to="/tenable">1</Link>
                 <Link to="/weakest-link">2</Link>
